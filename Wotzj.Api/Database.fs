5b3d236b8a043d931d5cfdc160ba7ca9ef3c8d6c
Eelco Mulder <eelcomulder@hotmail.com>
2018-02-10 20:15:21 +0100

    Remove Azure stuff from code. Reset password

--- Wotzj.Api/Database.fs
+++ Wotzj.Api/Database.fs
@@ -5,7 +5,7 @@ open FSharp.Data.Sql.Transactions
 
 module DatabaseContext =
     [<Literal>]
-    let ConnectionString = "Server=tcp:wotzjapi.database.windows.net,1433;Initial Catalog=wotzj;Persist Security Info=False;User ID=wotzjadmin;Password=Bdh8DQ$jH0@O0wRr;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-    type SeriesSqlProvider = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, ConnectionString>
+    let WotzjConnectionString = @"Server=.\SQLExpress;Database=Series;Integrated Security=SSPI;"
+    type SeriesSqlProvider = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, WotzjConnectionString>
     let getContext = 
         SeriesSqlProvider.GetDataContext( { Timeout = System.TimeSpan.MaxValue; IsolationLevel = IsolationLevel.DontCreateTransaction})
